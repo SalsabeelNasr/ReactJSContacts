56576a83dfbb2bfa462386d71f660300c3c20e5c
Salsabeel Nasr <salsabeelnasr@gmail.com>
2017-08-14 11:49:56 +0200

    moved contact segment in a separate component

--- src/app/states/contacts/component.js
+++ src/app/states/contacts/component.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 import cookie from '../../utils/cookies'
 import router from '../../app.router';
 import NavBar from '../navbar/component'
-
+import ContactSegment from '../contactSegment/component'
 export default class ContactsState extends React.Component {
   constructor(props) {
           super(props);
@@ -33,10 +33,7 @@ export default class ContactsState extends React.Component {
                     }).then(response=> {
                             return response.json();
                     }).then(json=>{
-                            this.setState( (state) => {
-                                        state.contacts = state.contacts.concat(json.data);
-                                        return state;
-                                    });
+                            this.setState({contacts:this.state.contacts.concat(json.data)});
                     }).catch(function(ex) {
                           console.log('failed', ex)
                     });
@@ -44,56 +41,7 @@ export default class ContactsState extends React.Component {
                   };
                 };
              render (){
-                        function  PhoneNumbers({ phoneNumbers }) {
-                                            const isNull = !phoneNumbers;
-                                            const isEmpty = !isNull && !phoneNumbers.length;
-                                            return (
-                                                <div>
-                                                    { isNull
-                                                        ? <span><span className='glyphicon glyphicon-earphone'></span> no phone number</span>
-                                                        : ( isEmpty
-                                                            ? <span><span className='glyphicon glyphicon-earphone'></span> no phone number</span>
-                                                            : <div>{phoneNumbers.filter((number, i) => (i < 1)).map(function(number,i){
-                                                                return(<span key={i}><span className='glyphicon glyphicon-earphone'></span>
-                                                                 {number.number}</span> ) })}</div>
-                                                        )
-                                                    }
-                                                </div>
-                                            );
-                                        }
-                        function ProfilePicture({profilePicture}){
-                                    return(
-                                        !profilePicture
-                                        ? <img src='https://techreport.com/forums/styles/canvas/theme/images/no_avatar.jpg' alt="No Avatar" className='img-responsive img-circle ' />
-                                        : <img src={profilePicture}  className='img-responsive img-circle ' />
-                                    )
-                        }
-                        function ContactType({contactType}){
-                                if(contactType=='COMPANY'){
-                                    return(<span className='pull-right glyphicon glyphicon-briefcase'></span>)
-                                    }else if(contactType=='PERSON'){
-                                        return(<span className='pull-right glyphicon glyphicon-user'></span>)
-                                    }
-                        }
-                        function Emails({ emails }) {
-                           const isNull = !emails;
-                           const isEmpty = !isNull && !emails.length;
-                           return (
-                               <div>
-                                   { isNull
-                                       ? <span><span className='glyphicon glyphicon-envelope'></span> no email</span>
-                                       : ( isEmpty
-                                           ? <span><span className='glyphicon glyphicon-envelope'></span>no email</span>
-                                           : <div>{emails.filter((email, i) => (i < 1)).map(function(email,i){
-                                               return(<span key={i}>
-                                                 <span className='glyphicon glyphicon-envelope'></span>
-                                                  {email.email}
-                                                  </span> ) })}</div>
-                                       )
-                                   }
-                               </div>
-                           );
-                         };
+
                      return (
                       <div>
                           <NavBar></NavBar>
@@ -105,21 +53,12 @@ export default class ContactsState extends React.Component {
                                             </div>
                                          <ul className='list-group' id='contact-list'>
                                            {
-                                                    this.state.contacts.map(function(contact,i){
+                                                    this.state.contacts.map(
+                                                      function(contact,i){
                                                         return(
-                                                           <li className='list-group-item ' key={contact.id}>
-                                                                <div className='col-xs-12 col-sm-3 '>
-                                                                  <ProfilePicture profilePicture={contact.profilePicture} />
-                                                               </div>
-                                                               <div className='col-xs-12 col-sm-9 '>
-                                                                   <ContactType contactType={contact.contactType}/>
-                                                                 <span  className='name'>{contact.displayName}</span><br/>
-                                                                     <span> <PhoneNumbers phoneNumbers={contact.phoneNumbers} /> </span>
-                                                                     <span> <Emails emails={contact.emails} /> </span>
-                                                                </div>
-                                                                <div className='clearfix '></div>
-                                                            </li>)
-                                                    })
+                                                          <ContactSegment key={contact.id} contact={contact}></ContactSegment>
+                                                    );
+                                                  })
                                            }
                                            <div className='panel-footer c-list'>
                                                  <div className='col-md-offset-9'><button className='btn btn-default'  onClick={this.handleLoadMore}>load more</button> </div>
